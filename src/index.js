622eb17b3e92a1de3b1235ad3e99e8e5275ecfa1
Zach Smith <zachsmith.sa@gmail.com>
2023-09-13 09:19:00 +0200

    fixed fetch hook to correctly escape non-localhost

--- src/index.js
+++ src/index.js
@@ -2,23 +2,23 @@ const { transform } = require('@babel/core')
 const presetReact = require('@babel/preset-react')
 
 async function compile(url, source) {
-  if (url.includes('http://localhost')) {
-    const transformed = transform(source, {
-      presets: [presetReact],
-    })
-    return transformed.code
-  }
-  return source.replace(/\/\/#.*/, '')
+  const transformed = transform(source, {
+    presets: [presetReact],
+  })
+  return transformed.code
 }
 
 globalThis.esmsInitOptions = {
   ...(globalThis.esmsInitOptions || {}),
-  shimMode: true,
+  shimMode: true, // This gets enabled by the presence of <script type="module-shim">
   async fetch(url, options) {
     const res = await fetch(url, options)
     if (!res.ok) return res
-    const source = await res.text()
-    const transformed = await compile(url, source)
-    return new Response(new Blob([transformed], { type: 'application/javascript' }))
+    if (url.includes('http://localhost')) {
+      const source = await res.text()
+      const transformed = await compile(url, source)
+      return new Response(new Blob([transformed], { type: 'application/javascript' }))
+    }
+    return res
   },
 }
